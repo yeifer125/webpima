61d759b8186040d542b36fe31659614210aeb541
cArbona0Ato <61042156+yeifer125@users.noreply.github.com>
2025-09-11 12:17:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-API_PIMA = os.environ.get("API_PIMA_URL", "https://apiparagit.onrender.com/precios")
+API_PIMA = os.environ.get("API_PIMA_URL", "https://apiparagit-3yxs.onrender.com/precios")
 
 # Middleware para mostrar la IP en logs
 @app.before_request
